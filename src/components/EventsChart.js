a41d7b6a8b0785b721e81d23288a1d04a542cba4
Joe Furfaro <joe@mrstache.io>
2024-01-17 09:34:43 -0800

    temp rough code to get charts displaying info

--- src/components/EventsChart.js
+++ src/components/EventsChart.js
@@ -1,25 +1,25 @@
+// TODO: this file desparately needs consolidation
+// - changes here were only to get the minimal charts working with disparate chart types returned
 import './EventsChart.less';
 
-import React, { useState, useMemo, memo } from 'react';
-import PropTypes from 'prop-types';
+import React, { useState, useMemo, memo, useContext, useEffect } from 'react';
+import EventContext from 'EventContext';
 import { format, set } from 'date-fns';
 import { Line } from 'react-chartjs-2';
 import { Chart as ChartJS, CategoryScale, LineElement, PointElement, LinearScale, Title, Tooltip } from 'chart.js';
+import apiClient from 'utils/apiClient';
+import Loader from 'components/Loader';
 
 ChartJS.register(CategoryScale, LineElement, PointElement, LinearScale, Title, Tooltip);
 
 const availableTypes = [
 	{
-		id: 'opening',
+		id: 'openingSales',
 		name: 'Opening Day Ticket Sales'
 	},
 	{
 		id: 'tickets',
 		name: 'Daily Ticket Sales'
-	},
-	{
-		id: 'ticketsAvg',
-		name: 'Daily Avg. Tickets/Transaction'
 	},
 	{
 		id: 'ticketsAcc',
@@ -32,9 +32,9 @@ const availableTypes = [
 ];
 
 const generateDailyTicketsSeries = chartData => ({
-	labels: chartData.dailySales.map(({ date }) => format(new Date(date), 'M/dd')),
+	labels: chartData.map(({ date }) => format(new Date(date), 'M/dd')),
 	datasets: [{
-		data: chartData.dailySales.map(({ tickets }) => tickets),
+		data: chartData.map(({ tickets }) => tickets),
 		label: `${chartData.name}: Daily Tickets Sold`,
 		lineTension: 0.3,
 		fill: false,
@@ -44,36 +44,10 @@ const generateDailyTicketsSeries = chartData => ({
 	}]
 });
 
-const generateOpeningDayTicketsSeries = chartData => ({
-	labels: chartData.openingDaySales.map(({ time }) => format(new Date(time), 'HH:mm')),
-	datasets: [{
-		data: chartData.openingDaySales.map(({ tickets }) => tickets),
-		label: `${chartData.name}: Tickets Sold ${format(chartData.openingDaySales[0]?.time ? new Date(chartData.openingDaySales[0].time) : new Date(), 'MMM do, yyyy')}`,
-		lineTension: 0.3,
-		fill: false,
-		borderColor: 'rgb(73, 134, 210)',
-		pointBorderColor: 'rgba(73, 134, 210, 0.5)',
-		pointBackgroundColor: 'rgba(73, 134, 210, 0.5)'
-	}]
-});
-
-const generateAvgTicketsPerTransactionSeries = chartData => ({
-	labels: chartData.dailySales.map(({ date }) => format(new Date(date), 'M/dd')),
-	datasets: [{
-		data: chartData.dailySales.map(({ tickets, transactions }) => Math.round((tickets/transactions)*100)/100), // two significant digits
-		label: `${chartData.name}: Avg. Tickets Per Transaction`,
-		lineTension: 0.3,
-		fill: false,
-		borderColor: 'rgb(184, 233, 134)',
-		pointBorderColor: 'rgba(184, 233, 134, 0.5)',
-		pointBackgroundColor: 'rgba(184, 233, 134, 0.5)'
-	}]
-});
-
 const generateTotalTicketsArea = chartData => ({
-	labels: chartData.dailySales.map(({ date }) => format(new Date(date), 'M/dd')),
+	labels: chartData.map(({ date }) => format(new Date(date), 'M/dd')),
 	datasets: [{
-		data: chartData.dailySales.reduce((acc, day) => {
+		data: chartData.reduce((acc, day) => {
 			acc.total += day.tickets;
 			acc.set.push(acc.total);
 
@@ -88,8 +62,22 @@ const generateTotalTicketsArea = chartData => ({
 	}]
 });
 
+const generateOpeningDayTicketsSeries = chartData => ({
+	labels: chartData.map(({ minuteCreated }) => format(new Date(minuteCreated), 'HH:mm')),
+	datasets: [{
+		data: chartData.map(({ tickets }) => tickets),
+		// label: `${chartData.name}: Tickets Sold ${format(chartData[0]?.time ? new Date(chartData[0].time) : new Date(), 'MMM do, yyyy')}`,
+		label: 'TICKETS',
+		lineTension: 0.3,
+		fill: false,
+		borderColor: 'rgb(73, 134, 210)',
+		pointBorderColor: 'rgba(73, 134, 210, 0.5)',
+		pointBackgroundColor: 'rgba(73, 134, 210, 0.5)'
+	}]
+});
+
 const generateCheckInsSeries = chartData => ({
-	labels: chartData.checkIns.map(({ hour, minutes }) => format(set(new Date(0), {hours: hour, minutes}), 'HH:mm')),
+	labels: chartData.map(({ hour, minutes }) => format(set(new Date(0), {hours: hour, minutes}), 'HH:mm')),
 	datasets: [{
 		data: chartData.checkIns.map(({ checkedIn }) => checkedIn),
 		label: `${chartData.name}: Check Ins`,
@@ -101,27 +89,55 @@ const generateCheckInsSeries = chartData => ({
 	}]
 });
 
-const EventsChart = ({ chartData }) => {
-	const [ graphType, setGraphType ] = useState('tickets');
+const EventsChart = () => {
+	const [ graphType, setGraphType ] = useState('tickets'),
+		[ ticketsChartData, setTicketsChartData ] = useState(null),
+		[ openingSalesChartData, setOpeningSalesChartData ] = useState(null),
+		[ checkInsChartData, setCheckInsChartData ] = useState(null),
+		{ event } = useContext(EventContext);
+
+	useEffect(() => {
+		if(event?.id) {
+			const type = graphType === 'ticketsAcc' ? 'tickets' : graphType;
+			apiClient.get(`/events/${event.id}/chart`, {type})
+				.then(data => {
+					switch(type) {
+						case 'openingSales':
+							return setOpeningSalesChartData(data);
+
+						case 'tickets':
+							return setTicketsChartData(data);
+
+						case 'checkIns':
+							return setCheckInsChartData(data);
+					}
+				})
+				.catch(e => console.error('Chart Data API Error', e));
+		}
+	}, [event.id, graphType]);
 
 	const data = useMemo(() => {
 		switch(graphType) {
-			case 'opening':
-				return generateOpeningDayTicketsSeries(chartData);
+			case 'openingSales':
+				return openingSalesChartData && generateOpeningDayTicketsSeries(openingSalesChartData);
 
 			case 'tickets':
-				return generateDailyTicketsSeries(chartData);
-
-			case 'ticketsAvg':
-				return generateAvgTicketsPerTransactionSeries(chartData);
-
-			case 'checkIns':
-				return generateCheckInsSeries(chartData);
+				return ticketsChartData && generateDailyTicketsSeries(ticketsChartData);
 
 			case 'ticketsAcc':
-				return generateTotalTicketsArea(chartData);
+				return ticketsChartData && generateTotalTicketsArea(ticketsChartData);
+
+			case 'checkIns':
+				return checkInsChartData && generateCheckInsSeries(checkInsChartData);
 		}
-	}, [chartData, graphType]);
+	}, [ticketsChartData, openingSalesChartData, checkInsChartData, graphType]);
+
+	if(
+		graphType === 'openingSales' && !openingSalesChartData ||
+		graphType === 'tickets' && !ticketsChartData ||
+		graphType === 'checkIns' && !checkInsChartData ||
+		graphType === 'ticketsAcc' && !ticketsChartData
+	) return <Loader />;
 
 	return (
 		<div className="events-chart">
@@ -140,10 +156,6 @@ const EventsChart = ({ chartData }) => {
 			</div>
 		</div>
 	);
-};
-
-EventsChart.propTypes = {
-	chartData: PropTypes.object.isRequired
 };
 
 export default memo(EventsChart);
